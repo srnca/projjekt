bbb9ad91e0723f42fce7b7f0376296aadd5a6205
Srnka, Roman <Roman.Srnka@softwareag.com>
2025-01-29 00:10:19 +0100

    Init template

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,101 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Script from 'next/script';
+import generateStylesheetObject from '@/common/generateStylesheetsObject';
+import Loader from '@/components/Loader';
+import Navbar from '@/components/Navbar';
+import Header from '@/components/Header';
+import Services from '@/components/Services';
+import Process from '@/components/Process';
+import Projects from '@/components/Projects';
+import Award from '@/components/Award';
+import Testimonials from '@/components/Testimonials';
+import Blog from '@/components/Blog';
+import Contact from '@/components/Contact';
+import Footer from '@/components/Footer';
 
+export const metadata = {
+  title: 'projjekt',
+  icons: {
+    icon: '/assets/img/fav.png',
+    shortcut: '/assets/img/fav.png',
+    other: generateStylesheetObject([
+      '/assets/css/lib/bootstrap.min.css',
+      '/assets/css/lib/all.min.css',
+      '/assets/css/lib/line-awesome.css',
+      '/assets/css/lib/themify-icons.css',
+      '/assets/css/lib/animate.css',
+      '/assets/css/lib/jquery.fancybox.css',
+      '/assets/css/lib/lity.css',
+      '/assets/css/lib/swiper8.min.css',
+      '/assets/css/common_style.css',
+      '/assets/css/home_2_style.css',
+      'https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap',
+      'https://fonts.googleapis.com/css2?family=Instrument+Serif:ital@0;1&display=swap',
+    ]),
+  },
+};
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+    <body className="home-style2">
+      <Loader />
+      <div id="smooth-wrapper">
+        <div id="smooth-content">
+          <Navbar />
+          <Header />
+          <main>
+            <Services />
+            <Process />
+            <Projects />
+            <Award />
+            <Testimonials />
+            <Blog />
+            <Contact />
+            {/*
+             */}
+          </main>
+          <Footer />
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </div>
+      <Script
+        src="/assets/js/ScrollTrigger.min.js"
+        strategy="beforeInteractive"
+      />
+      <Script
+        src="/assets/js/ScrollSmoother.min.js"
+        strategy="beforeInteractive"
+      />
+      <Script
+        strategy="beforeInteractive"
+        src="/assets/js/plugins.js"
+      ></Script>
+      <Script
+        strategy="beforeInteractive"
+        src="/assets/js/TweenMax.min.js"
+      ></Script>
+      <Script
+        strategy="beforeInteractive"
+        src="/assets/js/charming.min.js"
+      ></Script>
+      <Script
+        strategy="beforeInteractive"
+        src="/assets/js/countdown.js"
+      ></Script>
+      <Script
+        strategy="beforeInteractive"
+        src="/assets/js/gsap.min.js"
+      ></Script>
+      <Script
+        strategy="beforeInteractive"
+        src="/assets/js/splitting.min.js"
+      ></Script>
+      <Script
+        strategy="beforeInteractive"
+        src="/assets/js/isotope.pkgd.min.js"
+      ></Script>
+      <Script
+        src="/assets/js/smoother-script.js"
+        strategy="lazyOnload"
+      />
+      <Script src="/assets/js/scripts.js"></Script>
+    </body>
   );
 }
