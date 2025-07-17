e735f3bf6fdcc5625f1ca449b4131a4ce3f5e536
Srnka, Roman <Roman.Srnka@softwareag.com>
2025-07-18 00:55:22 +0200

    Fix build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,6 @@ import Header from '@/components/Header';
 import Services from '@/components/Services';
 import Process from '@/components/Process';
 import Projects from '@/components/Projects';
-import Award from '@/components/Award';
-import Testimonials from '@/components/Testimonials';
-import Blog from '@/components/Blog';
 import Contact from '@/components/Contact';
 import Footer from '@/components/Footer';
 
